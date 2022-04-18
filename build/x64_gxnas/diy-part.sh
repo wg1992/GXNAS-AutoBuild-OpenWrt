7f49d11dbca99a5ad0abb5dbcea5f368ff91287d
gxnas <52808122+gxnas@users.noreply.github.com>
2022-04-18 09:08:27 +0800

    Update diy-part.sh

--- build/x64_gxnas/diy-part.sh
+++ build/x64_gxnas/diy-part.sh
@@ -14,6 +14,10 @@ svn co https://github.com/linkease/istore-ui/tree/main/app-store-ui package/stor
 # 添加ikoolproxy广告过滤
 git clone https://github.com/1wrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
 
+# 添加解除网易云音乐播放限制
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
+
+
 cat >$NETIP <<-EOF
 uci set network.lan.ipaddr='192.168.1.11'                      # IPv4 地址(openwrt后台地址)
 uci set network.lan.netmask='255.255.255.0'                   # IPv4 子网掩码
