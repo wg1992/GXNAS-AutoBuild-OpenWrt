14efccfe2efe0607fa3f15abf1e77dc15ab02646
gxnas <52808122+gxnas@users.noreply.github.com>
2022-04-24 19:00:18 +0800

    Update diy-part.sh

--- build/x64_test/diy-part.sh
+++ build/x64_test/diy-part.sh
@@ -15,8 +15,8 @@ svn co https://github.com/linkease/istore-ui/tree/main/app-store-ui package/stor
 git clone https://github.com/1wrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
 
 # 添加解除网易云音乐播放限制
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
-
+#git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
+git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
 
 cat >$NETIP <<-EOF
 uci set network.lan.ipaddr='192.168.1.11'                     # IPv4 地址(openwrt后台地址)
