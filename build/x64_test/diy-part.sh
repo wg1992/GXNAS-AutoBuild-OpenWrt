79a31da86715396e1599b685596b4c3f0ca13e1e
gxnas <52808122+gxnas@users.noreply.github.com>
2022-08-06 00:41:58 +0800

    Update diy-part.sh

--- build/x64_test/diy-part.sh
+++ build/x64_test/diy-part.sh
@@ -7,10 +7,6 @@
 # 如果你的OP是当主路由的话，网关、DNS、广播都不需要，代码前面加 # 注释掉，只保留后台地址和子网掩码就可以
 # 如果你有编译ipv6的话，‘去掉LAN口使用内置的 IPv6 管理’代码前面也加 # 注释掉
 
-#添加kenzok8插件包
-git clone https://github.com/waynesg/OpenWrt-Software package/x64_wjq-Software
-
-
 cat >$NETIP <<-EOF
 uci set network.lan.ipaddr='192.168.1.11'                     # IPv4 地址(openwrt后台地址)
 uci set network.lan.netmask='255.255.255.0'                   # IPv4 子网掩码
