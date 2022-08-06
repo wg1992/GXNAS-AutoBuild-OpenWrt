6b61347a0d46efef0ebe556b1ae1671149a7d06a
gxnas <52808122+gxnas@users.noreply.github.com>
2022-08-06 12:21:11 +0800

    Update diy-part.sh

--- build/x64_all/diy-part.sh
+++ build/x64_all/diy-part.sh
@@ -7,11 +7,8 @@
 # 如果你的OP是当主路由的话，网关、DNS、广播都不需要，代码前面加 # 注释掉，只保留后台地址和子网掩码就可以
 # 如果你有编译ipv6的话，‘去掉LAN口使用内置的 IPv6 管理’代码前面也加 # 注释掉
 
-# 添加ikoolproxy广告过滤
-#git clone https://github.com/1wrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
-
-# 添加解除网易云音乐播放限制
-#git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
+#添加kenzok8插件包
+#git clone https://github.com/waynesg/OpenWrt-Software package/x64_gxnas-Software
 
 cat >$NETIP <<-EOF
 #uci set network.lan.ipaddr='192.168.1.1'                     # IPv4 地址(openwrt后台地址)
