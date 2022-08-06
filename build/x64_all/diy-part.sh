2c106417c387c24dc21dd2d66ef0d1f76f4f2e23
gxnas <52808122+gxnas@users.noreply.github.com>
2022-08-06 08:55:29 +0800

    Update diy-part.sh

--- build/x64_all/diy-part.sh
+++ build/x64_all/diy-part.sh
@@ -8,10 +8,10 @@
 # 如果你有编译ipv6的话，‘去掉LAN口使用内置的 IPv6 管理’代码前面也加 # 注释掉
 
 # 添加ikoolproxy广告过滤
-git clone https://github.com/1wrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
+#git clone https://github.com/1wrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
 
 # 添加解除网易云音乐播放限制
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
+#git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
 
 cat >$NETIP <<-EOF
 #uci set network.lan.ipaddr='192.168.1.1'                     # IPv4 地址(openwrt后台地址)
