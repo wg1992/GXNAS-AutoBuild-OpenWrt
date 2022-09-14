1c9c0b4dc398b0166eaead9a2abdc09fba89f88d
gxnas <52808122+gxnas@users.noreply.github.com>
2022-09-14 22:07:13 +0800

    Update diy-part.sh

--- build/x64_jd/diy-part.sh
+++ build/x64_jd/diy-part.sh
@@ -7,9 +7,6 @@
 # 如果你的OP是当主路由的话，网关、DNS、广播都不需要，代码前面加 # 注释掉，只保留后台地址和子网掩码就可以
 # 如果你有编译ipv6的话，‘去掉LAN口使用内置的 IPv6 管理’代码前面也加 # 注释掉
 
-#添加bypass插件包
-git clone https://github.com/kiddin9/openwrt-bypass package/x64_gxnas-Software
-
 # 添加京东脚本
 git clone https://github.com/xdhgsq/xdh_plug.git package/jd_openwrt_script
 
